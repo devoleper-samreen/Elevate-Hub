cafa45e11d425c85d7cfb86eba3403ef096030b5
devoleper-samreen <samreenmalik52292@gmail.com>
2025-03-15 21:35:11 +0530

    feat: implement createService function; add service creation logic with error handling and response messages

--- Backend/src/controllers/service.controller.js
+++ Backend/src/controllers/service.controller.js
@@ -1,3 +1,33 @@
-export const createService = async () => {
+import { Service } from "../models/service.model.js"
+import { httpStatus } from "../utils/httpStatus.js"
+import { ApiError } from "../helpers/apiError.js"
+
+export const createService = async (req, res,) => {
+    try {
+        const mentorId = req.user._id
+        const { serviceName, description, duration, price } = req.body
+
+        const service = await Service.create({
+            mentor: mentorId,
+            serviceName,
+            description,
+            duration,
+            price
+        })
+
+        if (!service) {
+            return res.status(httpStatus.internalServerError).json({
+                message: "Service not created!"
+            })
+        }
+
+        return res.status(httpStatus.ok).json({
+            message: "Service created successfully!",
+            service
+        })
+
+    } catch (error) {
+        throw new ApiError(httpStatus.internalServerError, "Interner Server Error")
+    }
 
 }
